45d4213603df1c8a0f4a848e4ab6ffd0c56f0e41
TH-project <project.th.withai@gmail.com>
2026-08-25 14:25:53 +0900

    feat: Initialize Apex Suite Math Lab with rules and dependencies

--- lib/liquidTimer.ts
+++
@@ -1,4 +0,0 @@
-// @ts-nocheck
-export const DEFAULT_MAX_WORK_MIN = 50;
-export const DEFAULT_WORK_REST_RATIO = 5;
-export const DEFAULT_LONG_BREAK_MIN = 15;
--- lib/timerLogs.ts
+++
@@ -1,4 +0,0 @@
-// @ts-nocheck
-export const logTimerSession = async (data: any) => {
-  console.log("Timer session logged:", data);
-};
